bbda9d462e99ae308f982f0d57314a1d317b37cb
Mehar-Aziz <meharaziz.1614>
2023-12-10 16:51:47 +0500

    Updated routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const router = express.Router();
+const Model = require('../models/model');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
