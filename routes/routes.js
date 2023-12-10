618ccf19a2676185189d97689cc2ba62d048d2dd
Mehar-Aziz <meharaziz.1614>
2023-12-10 16:26:36 +0500

    Added Routes

---
+++ routes/routes.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+const booksData = require('./booksData');
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect('mongodb://localhost:27017/bookstore', { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+
+const Book = mongoose.model('Book', bookSchema);
+
+app.use(bodyParser.json());
+
+app.get('/api/books', async (req, res) => {
+  try {
+    const books = await Book.find({});
+    res.json(booksData);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+app.post('/api/books', async (req, res) => {
+  const book = new Book({
+    title: req.body.title,
+    author: req.body.author,
+    genre: req.body.genre,
+  });
+
+  try {
+    const newBook = await book.save();
+    res.status(201).json(newBook);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
