98315db4438e8f3444f1c9abfc8ef1511a1fe677
Mehar-Aziz <meharaziz.1614>
2023-12-10 16:32:07 +0500

    Added script

---
+++ script.js
@@ -0,0 +1,43 @@
+const bookForm = document.getElementById('bookForm');
+const bookList = document.getElementById('bookList');
+
+async function fetchBooks() {
+  try {
+    const response = await fetch('/api/books');
+    const books = await response.json();
+
+    bookList.innerHTML = '';
+    books.forEach(book => {
+      const div = document.createElement('div');
+      div.innerHTML = `<h3>${book.title}</h3><p>Author: ${book.author}</p><p>Genre: ${book.genre}</p>`;
+      bookList.appendChild(div);
+    });
+  } catch (err) {
+    console.error('Error fetching books:', err);
+  }
+}
+
+bookForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const genre = document.getElementById('genre').value;
+
+  const bookData = { title, author, genre };
+
+  try {
+    await fetch('/api/books', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(bookData),
+    });
+    fetchBooks();
+  } catch (err) {
+    console.error('Error adding book:', err);
+  }
+});
+
+fetchBooks();
